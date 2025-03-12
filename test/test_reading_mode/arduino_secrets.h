799c0a25fc7b59ab027a92dc3b03d5e72e9c9db1
Yang Wenhao <1718967807@qq.com>
2025-03-12 16:41:27 +0000

    Update arduino_secrets.h

--- test/test_reading_mode/arduino_secrets.h
+++ test/test_reading_mode/arduino_secrets.h
@@ -1,4 +1,4 @@
-#define SECRET_SSID "A9BF Hyperoptic 1Gb Fibre 2.4Ghz"
-#define SECRET_PASS "gS42TdrXG9KY"
+#define SECRET_SSID "CE-Hub-Student"
+#define SECRET_PASS "casa-ce-gagarin-public-service"
 #define SECRET_MQTTUSER "student"
 #define SECRET_MQTTPASS "ce2021-mqtt-forget-whale";
